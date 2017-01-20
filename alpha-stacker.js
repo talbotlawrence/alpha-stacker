2cfb06848b9608e8e4827d65fa6dd568531cedbf
Talbot <talbotlawrence75@gmail.com>
2017-01-20 10:41:34 -0600

    finishing alpha-stacker

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -1,13 +1,12 @@
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t",
 "u","v","w","x","y","z"]
 
-function stackLetters (theAlphabetArray) {
-
+function stackLetters (alph) {
+	var stack = "";	
+		for (var i = 0; i < alph.length; i++) {		
+		stack += alph[i];	
+		console.log(stack);			
+	}
 }
 
-stackLetters(alphabet)
-
-
-for (var i = 0; i < alphabet.length; i++) {
-	console.log("alphabet", alphabet);
-}
+stackLetters(alphabet);

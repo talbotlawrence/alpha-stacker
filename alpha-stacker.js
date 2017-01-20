fad145950e6b7633c860d67b2c6ed0ff94572bbd
Talbot <talbotlawrence75@gmail.com>
2017-01-20 10:01:33 -0600

    first commit

---
+++ alpha-stacker.js
@@ -0,0 +1,13 @@
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t",
+"u","v","w","x","y","z"]
+
+function stackLetters (theAlphabetArray) {
+
+}
+
+stackLetters(alphabet)
+
+
+for (var i = 0; i < alphabet.length; i++) {
+	console.log("alphabet", alphabet);
+}
